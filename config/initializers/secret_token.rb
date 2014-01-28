af2b1e4d88c63d38c76799a28000bec9ac59228f
benjamin <benjamin@benjamin-1005HA.(none)>
2014-01-28 12:46:57 -0800

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fremote::Application.config.secret_key_base = '976822ec5fb63e9bd67a259752556988e4b5a42b71c9e6626c5ef450a8e4d3b09496dd4cc38e3286101ad760156fbf22a569c8b4e1ab29249dda16f78711ffb7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fremote::Application.config.session_store :cookie_store, key: '_fremote_session'

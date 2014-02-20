4ff8e547abc9661b3ebd38f986a0d11b6bd7361c
benjamin <benjamin@benjamin-1005HA.(none)>
2014-02-20 15:29:30 -0800

    Improve navbar style.

--- app/assets/javascripts/ui.js
+++ app/assets/javascripts/ui.js
@@ -1,3 +1,15 @@
 $(document).on('ready', function(){
   $('#owner_only_tooltip').tooltip()
+
+    var nav = $('#top_nav')
+
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 3) {
+            nav.addClass("nav-scrolled")
+        } else {
+            nav.removeClass("nav-scrolled")
+        }
+    })
+
 })

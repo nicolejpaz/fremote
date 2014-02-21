e5197b30b4e8ec96dd675470b9faf85e18cf6c6c
benjamin <benjamin@benjamin-1005HA.(none)>
2014-02-20 20:32:42 -0800

    Remake validation to make way for using Viddl strictly.

--- app/models/remote.rb
+++ app/models/remote.rb
@@ -2,27 +2,13 @@ class Remote
   include Rails.application.routes.url_helpers
   include Mongoid::Document
   include Mongoid::Timestamps
-  field :video_id, type: String
   field :remote_id, type: String
-  field :provider, type: String
-  field :title, type: String
-  field :description, type: String
-  field :duration, type: Integer
-  field :thumbnail_small, type: String
-  field :thumbnail_medium, type: String
-  field :thumbnail_large, type: String
   field :url, type: String
-  field :embed_url, type: String
-  field :embed_code, type: String
-  field :date, type: DateTime
   field :status, type: Integer, default: -1
   field :start_at, type: Integer, default: 0
   field :admin_only, type: Boolean, default: false
   belongs_to :user
-  validates_presence_of :video_id
-  validates_presence_of :provider
   validates_presence_of :status
-  validates_presence_of :duration
   validates_presence_of :start_at
 
   def to_param
@@ -31,24 +17,11 @@ class Remote
 
   def populate(url)
     unless url == ""
-
-      begin
-        video = VideoInfo.new(url)
-        self.video_id = video.video_id
-        self.provider = video.provider
-        self.title = video.title
-        self.description = video.description
-        self.duration = video.duration
-        self.thumbnail_small = video.thumbnail_small
-        self.thumbnail_medium = video.thumbnail_medium
-        self.thumbnail_large = video.thumbnail_large
-        self.url = video.url
-        self.embed_url = video.embed_url
-        self.embed_code = video.embed_code
-        self.date = video.date
-        self.remote_id = Digest::MD5.hexdigest(video_id + DateTime.now.to_s + DateTime.now.nsec.to_s).slice(0..9)
+      if url[/(youtube.com|vimeo.com)/] != nil
+        self.url = url
+        self.remote_id = Digest::MD5.hexdigest(url + DateTime.now.to_s + DateTime.now.nsec.to_s).slice(0..9)
         return { message: "Congratulations!  Take control of your remote.", status: :notice, path: remote_path(self.remote_id)}
-      rescue
+      else
         return { message: "Invalid URL", status: :alert, path: root_path }
       end
     else

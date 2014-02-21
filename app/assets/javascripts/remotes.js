4421b058c4078bb136c4bb70ac0c480ecb545a00
benjamin <benjamin@benjamin-1005HA.(none)>
2014-02-21 13:46:37 -0800

    Make playlist switch between videos on both toggle and video end.
    Former-commit-id: 7af87bf3f952f01ef1b0de17f310191d191871c5

--- app/assets/javascripts/remotes.js
+++ app/assets/javascripts/remotes.js
@@ -1,82 +1,99 @@
 
-    Remote.update = function(){
-      $.ajax({
-        type: 'POST',
-        url: '/remotes/' + Remote.remote_id,
-        data: { _method:'PUT', status: Remote.status, start_at: Remote.start_at, sender_id: user },
-        dataType: 'JSON'
-      })
-    }
+Remote.update = function(){
+  $.ajax({
+    type: 'POST',
+    url: '/remotes/' + Remote.remote_id,
+    data: { _method:'PUT', status: Remote.status, start_at: Remote.start_at, sender_id: user },
+    dataType: 'JSON'
+  })
+}
 
-    Remote.ping = function(){
-      $.ajax({
-        type: 'GET',
-        url: '/remotes/' + Remote.remote_id + "/ping"
-      })
-    }
+Remote.ping = function(){
+  $.ajax({
+    type: 'GET',
+    url: '/remotes/' + Remote.remote_id + "/ping"
+  })
+}
+
+Remote.pause = function(start_at){
+  player.currentTime(start_at)
+  player.play() // to bypass the big button mode
+  player.pause()
+}
+
+Remote.play = function(start_at, updated_at){
+  var offset = Math.max(0, (this.date - Date.parse(updated_at)) / 1000 )
+  player.currentTime(Math.floor(start_at + offset))
+  player.play()     
+}
+
+Remote.toggle = function(data){
+  if (data.status == -1 || data.status == 2){
+    this.pause(data.start_at)
+  } else if (data.status == 1){  
+    this.play(data.start_at, data.updated_at)
+  }
+}
 
 // Synchronize time with server.  Use this instead of Date.now().
-    $.ajax({
-      type: 'GET',
-      url: '/time',
-      async: false,
-      dataType: 'JSON'
-    }).done(function(response){
-      Remote.date = Date.parse(response.time)
-      setInterval(function(){Remote.date = Remote.date + 1000},1000);
-    })
+$.ajax({
+  type: 'GET',
+  url: '/time',
+  async: false,
+  dataType: 'JSON'
+}).done(function(response){
+  Remote.date = Date.parse(response.time)
+  setInterval(function(){Remote.date = Remote.date + 1000},1000);
+})
 
-    var send = true
+var player = videojs('player')
 
-    var player = videojs('player')
+player.ready(function(){
 
-    player.ready(function(){
+  var source = new EventSource(Remote.remote_id + '/stream')
 
-      var source = new EventSource(Remote.remote_id + '/stream')
-
-      source.addEventListener("control:" + Remote.remote_id, function(event){
-        var data = JSON.parse(event.data)
-        console.log(data)
-        console.log(user)
-        console.log(data.sender_id == user)
-        if (data.status == -1 || data.status == 2){
-          player.currentTime(data.start_at)
-          player.play() // to bypass the big button mode
-          player.pause()
-        } else if (data.status == 1){
-          var offset = Math.max(0, (Remote.date - Date.parse(data.updated_at)) / 1000 )
-          player.currentTime(Math.floor(data.start_at + offset))
-          player.play()     
-        }
+  source.addEventListener("control:" + Remote.remote_id, function(event){
+    var data = JSON.parse(event.data)
+    console.log(data)
+    if (data.stream_url != undefined){
+      player.src(data.stream_url)
+      player.one('loadedmetadata', function(){
+        Remote.toggle(data)
       })
-
-      source.addEventListener("chat:" + Remote.remote_id, function(event){
-        var data = JSON.parse(event.data)
-        console.log(data)
-        $('#chat_message').val('')
-        $('#chat_table_body').prepend('<tr>' + '<td>' + data.message + '</td>' + '<td class="grey-text">' + data.name + '</td>' + '</tr>')
-      })
-
-      $(document).on('userplay', function(){
-        Remote.status = 1
-        Remote.start_at = player.currentTime()
-        Remote.update()
-      })
-
-      $(document).on('userpause', function(){
-        Remote.status = 2
-        Remote.start_at = player.currentTime()
-        Remote.update()
-      })
-
-      player.on('timeupdate', function(){
-        player.loadingSpinner.hide()
-      })
-
-
-
-    })
-
-  player.on('loadedmetadata', function(){
-      Remote.ping()    
+    } else {
+      Remote.toggle(data)      
+    }
   })
+
+  source.addEventListener("chat:" + Remote.remote_id, function(event){
+    var data = JSON.parse(event.data)
+    $('#chat_message').val('')
+    $('#chat_table_body').prepend('<tr>' + '<td>' + data.message + '</td>' + '<td class="grey-text">' + data.name + '</td>' + '</tr>')
+  })
+
+  $(document).on('userplay', function(){
+    Remote.status = 1
+    Remote.start_at = player.currentTime()
+    Remote.update()
+  })
+
+  $(document).on('userpause', function(){
+    Remote.status = 2
+    Remote.start_at = player.currentTime()
+    Remote.update()
+  })
+
+  player.on('ended', function(){
+    Remote.status = 0
+    Remote.update()
+  })
+
+  player.on('timeupdate', function(){
+    player.loadingSpinner.hide()
+  })
+
+})
+
+player.ready(function(){
+  Remote.ping()    
+})
--- app/models/remote.rb
+++ app/models/remote.rb
@@ -7,6 +7,8 @@ class Remote
   field :status, type: Integer, default: -1
   field :start_at, type: Integer, default: 0
   field :admin_only, type: Boolean, default: false
+  field :playlist, type: Array, default: []
+  field :selection, type: Integer, default: 0
   belongs_to :user
   validates_presence_of :status
   validates_presence_of :start_at
@@ -16,9 +18,12 @@ class Remote
   end
 
   def populate(url)
+    new_video = {}
     unless url == ""
       if url[/(youtube.com|vimeo.com)/] != nil
-        self.url = url
+        new_video["url"] = url
+        new_video["title"] = ViddlRb.get_names(url).first
+        self.playlist << new_video
         self.remote_id = Digest::MD5.hexdigest(url + DateTime.now.to_s + DateTime.now.nsec.to_s).slice(0..9)
         return { message: "Congratulations!  Take control of your remote.", status: :notice, path: remote_path(self.remote_id)}
       else
@@ -39,11 +44,29 @@ class Remote
 
   def update(params, remote_owner = nil)
     if self.admin_only == false || remote_owner
+
       self.status = params["status"] if params["status"]
       self.start_at = params["start_at"].to_i if params["start_at"]
       self.admin_only = to_boolean(params["remote"]["admin_only"]) if params.has_key?("remote") && params["remote"].has_key?("admin_only")
-      self.save
-      ActiveSupport::Notifications.instrument("control:#{self.remote_id}", {'start_at' => self.start_at, 'status' => self.status, 'updated_at' => self.updated_at, 'dispatched_at' => Time.now, 'sender_id' => params['sender_id'] }.to_json)
+      
+      if params.has_key?("selection")
+        self.selection = params["selection"].to_i
+        self.start_at = 0
+        self.status = 1
+        self.save
+        ActiveSupport::Notifications.instrument("control:#{self.remote_id}", {'start_at' => self.start_at, 'status' => self.status, 'updated_at' => self.updated_at, 'dispatched_at' => Time.now, 'sender_id' => params['sender_id'], 'stream_url' => URI::encode(ViddlRb.get_urls(self.playlist[self.selection]["url"]).first) }.to_json)
+      elsif params["status"] == 0 || params["status"] == "0"
+        self.selection = (self.selection + 1) unless ((self.selection + 1) > (self.playlist.count - 1))
+        self.start_at = 0
+        self.status = 1
+        self.save
+        ActiveSupport::Notifications.instrument("control:#{self.remote_id}", {'start_at' => self.start_at, 'status' => self.status, 'updated_at' => self.updated_at, 'dispatched_at' => Time.now, 'sender_id' => params['sender_id'], 'stream_url' => URI::encode(ViddlRb.get_urls(self.playlist[self.selection]["url"]).first)  }.to_json)
+      else
+        self.save
+        ActiveSupport::Notifications.instrument("control:#{self.remote_id}", {'start_at' => self.start_at, 'status' => self.status, 'updated_at' => self.updated_at, 'dispatched_at' => Time.now, 'sender_id' => params['sender_id']}.to_json)
+      end
+
+
     end
   end
 

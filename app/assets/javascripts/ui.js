51ddc91eb53e8c53dd8af658e03b3dbbd1fcff75
Nicole Paz <nicole@nic.gateway.2wire.net>
2014-02-21 15:51:33 -0800

    Removed spacing issue in ui.js.

--- app/assets/javascripts/ui.js
+++ app/assets/javascripts/ui.js
@@ -1,15 +1,13 @@
 $(document).on('ready', function(){
   $('#owner_only_tooltip').tooltip()
 
-    var nav = $('#top_nav')
+  var nav = $('#top_nav')
 
-    
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 3) {
-            nav.addClass("nav-scrolled")
-        } else {
-            nav.removeClass("nav-scrolled")
-        }
-    })
-
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 3) {
+          nav.addClass("nav-scrolled")
+      } else {
+          nav.removeClass("nav-scrolled")
+      }
+  })
 })

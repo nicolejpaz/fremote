4637ea2f5433fd7530bb03ec25a647ab8842c322
benjamin <benjamin@benjamin-1005HA.(none)>
2014-02-21 18:06:09 -0800

    Make playlist draggable in the ui.

--- app/assets/javascripts/playlist.js
+++ app/assets/javascripts/playlist.js
@@ -1,12 +1,13 @@
 $(document).on('ready', function(){
-	$('#playlist_table').on('click', '.playlist_item', function(){
+	$('#playlist').on('click', '.playlist_item', function(){
 		var self = $(this)
       $.ajax({
         type: 'POST',
         url: '/remotes/' + Remote.remote_id,
-        data: { _method:'PUT', status: 0, start_at: Remote.start_at, sender_id: user, selection: self.closest('td').parent()[0].sectionRowIndex},
+        data: { _method:'PUT', status: 0, start_at: Remote.start_at, sender_id: user, selection: $('#playlist li').index(self)},
         dataType: 'JSON'
       })
+      console.log(self.closest('li').parent()[0].sectionRowIndex)
       console.log('playlist_item_clicked')
 	})
 })

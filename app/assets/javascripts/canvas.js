0efc182df2024f221bbbc7afcab667d99a5c89c0
Nicole Paz <nicole@nic.local>
2014-02-22 16:08:07 -0800

    Added ability to send coordinates in chunks.
    Former-commit-id: ee21ac7907966421d24b9f39eea1bc25e7c8c5d8

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -59,19 +59,31 @@ function draw_on_canvas(drawing_canvas, context, x_coordinate, y_coordinate) {
 
       current_coordinates.push({'x_coordinate': pos.x, 'y_coordinate': pos.y})
 
+      if (current_coordinates.length >= 20) {
+        send_coordinates(current_coordinates)
+
+        var new_current = current_coordinates[current_coordinates.length-1]
+        current_coordinates = []
+        current_coordinates.push(new_current)
+      }
     }
   }
 
   drawing_canvas.onmouseup = function(e) {
     mousedown = false
-    $.ajax({
-      type: 'POST',
-      url: '/remotes/' + Remote.remote_id + '/drawing',
-      data: {'coordinates': current_coordinates}
-    })
+    
+    send_coordinates(current_coordinates)
 
     current_coordinates = []
   }
+}
+
+function send_coordinates(current_coordinates) {
+  $.ajax({
+    type: 'POST',
+    url: '/remotes/' + Remote.remote_id + '/drawing',
+    data: {'coordinates': current_coordinates}
+  })
 }
 
 function getMousePos(drawing_canvas, e) {

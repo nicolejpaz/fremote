83490fd6a6946db1a7a3ad24df558d1fd56540d2
Nicole Paz <nicole@nic.gateway.2wire.net>
2014-02-24 18:53:48 -0800

    Added canvas drawing to Canvas object.

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-  var canvas = $('canvas')
+  var localCanvas = $('canvas')
 
-  var drawing = new Canvas(canvas)
+  var canvas = new Canvas(localCanvas)
 
-  console.log(drawing.context)
-
-  // draw(drawing_canvas[0])
+  canvas.draw()
 
   // $('button#clear').on('click', function(e) {
   //   clear()
@@ -17,7 +15,60 @@ $(document).ready(function() {
 })
 
 var Canvas = function(canvas) {
+  this.canvas = canvas
   this.context = canvas[0].getContext('2d')
+}
+
+Canvas.prototype.color = function() {
+  var color = $('input#color').val()
+
+  $('input#color').on('change', function(e) {
+    color = $('input#color').val()
+  })
+  return color
+}
+
+var mousedown = false
+
+function onMouseDown(targetCanvas) {
+  targetCanvas.onmousedown = function(e) {
+    var pos = getMousePos(targetCanvas, e)
+    mousedown = true
+    return false
+  }
+}
+
+Canvas.prototype.draw = function() {
+  var color = this.color()
+
+  var targetCanvas = this.canvas[0]
+
+  onMouseDown(targetCanvas)
+
+  var currentCoordinates = []
+
+  targetCanvas.onmousemove = function(e) {
+    e.preventDefault()
+    var pos = getMousePos(targetCanvas, e)
+
+    if (mousedown) {
+      currentCoordinates.push({'x_coordinate': pos.x, 'y_coordinate': pos.y, 'color': color})
+
+      if (currentCoordinates.length >= 10) {
+        sendCoordinates(currentCoordinates)
+
+        var new_current = [currentCoordinates[currentCoordinates.length-1]]
+        currentCoordinates = new_current
+      }
+    }
+  }
+
+  targetCanvas.onmouseup = function(e) {
+    mousedown = false
+    
+    sendCoordinates(currentCoordinates)
+    currentCoordinates = []
+  }
 }
 
 // function clear() {
@@ -25,87 +76,41 @@ var Canvas = function(canvas) {
 //   drawing_canvas.width = drawing_canvas.width
 // }
 
-// function draw(drawing_canvas, x_coordinate, y_coordinate) {
-//   var context = drawing_canvas.getContext('2d')
-//   draw_on_canvas(drawing_canvas, context, x_coordinate, y_coordinate)
-// }
+function remote_draw(previous_coordinates, x_coordinate, y_coordinate, color) {
+  var remote_canvas = $('canvas')[0]
+  var context = remote_canvas.getContext('2d')
 
-// function remote_draw(previous_coordinates, x_coordinate, y_coordinate, color) {
-//   var remote_canvas = $('canvas')[0]
-//   var context = remote_canvas.getContext('2d')
+  context.strokeStyle = color
+  context.lineWidth = 5
 
-//   context.strokeStyle = color
-//   context.lineWidth = 5
+  if (x_coordinate != null) {
 
-//   if (x_coordinate != null) {
+    var length = previous_coordinates.length - 1
+    var prev_x = parseInt(previous_coordinates[length]['x_coordinate'])
+    var prev_y = parseInt(previous_coordinates[length]['y_coordinate'])
 
-//     var length = previous_coordinates.length - 1
-//     var prev_x = parseInt(previous_coordinates[length]['x_coordinate'])
-//     var prev_y = parseInt(previous_coordinates[length]['y_coordinate'])
+    var x = parseInt(x_coordinate)
+    var y = parseInt(y_coordinate)
 
-//     var x = parseInt(x_coordinate)
-//     var y = parseInt(y_coordinate)
+    context.beginPath()
+    context.moveTo(prev_x, prev_y)
+    context.lineTo(x, y)
+    context.stroke()
+  }
+}
 
-//     context.beginPath()
-//     context.moveTo(prev_x, prev_y)
-//     context.lineTo(x, y)
-//     context.stroke()
-//   }
-// }
+function sendCoordinates(currentCoordinates) {
+  $.ajax({
+    type: 'POST',
+    url: '/remotes/' + Remote.remote_id + '/drawing',
+    data: {'coordinates': currentCoordinates}
+  })
+}
 
-// function draw_on_canvas(drawing_canvas, context, x_coordinate, y_coordinate) {
-//   var mousedown = false
-//   var color = $('input#color').val()
-
-//   $('input#color').on('change', function(e) {
-//     color = $('input#color').val()
-//   })
-
-//   drawing_canvas.onmousedown = function(e) {
-//     var pos = getMousePos(drawing_canvas, e)
-
-//     mousedown = true
-//     return false
-//   }
-
-//   var current_coordinates = []
-
-//   drawing_canvas.onmousemove = function(e) {
-//     e.preventDefault()
-//     var pos = getMousePos(drawing_canvas, e)
-
-//     if (mousedown) {
-//       current_coordinates.push({'x_coordinate': pos.x, 'y_coordinate': pos.y, 'color': color})
-
-//       if (current_coordinates.length >= 10) {
-//         send_coordinates(current_coordinates)
-
-//         var new_current = [current_coordinates[current_coordinates.length-1]]
-//         current_coordinates = new_current
-//       }
-//     }
-//   }
-
-//   drawing_canvas.onmouseup = function(e) {
-//     mousedown = false
-    
-//     send_coordinates(current_coordinates)
-//     current_coordinates = []
-//   }
-// }
-
-// function send_coordinates(current_coordinates) {
-//   $.ajax({
-//     type: 'POST',
-//     url: '/remotes/' + Remote.remote_id + '/drawing',
-//     data: {'coordinates': current_coordinates}
-//   })
-// }
-
-// function getMousePos(drawing_canvas, e) {
-//   var rect = drawing_canvas.getBoundingClientRect()
-//   return {
-//     x: e.clientX - rect.left,
-//     y: e.clientY - rect.top
-//   }
-// }
+function getMousePos(drawing_canvas, e) {
+  var rect = drawing_canvas.getBoundingClientRect()
+  return {
+    x: e.clientX - rect.left,
+    y: e.clientY - rect.top
+  }
+}

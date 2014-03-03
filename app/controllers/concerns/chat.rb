4cf9ff453292073e8cba59c3bc5914cb8ee4e0dd
benjamin <benjamin@benjamin.(none)>
2014-03-02 18:56:05 -0800

    Add guest name form for guests.
    Former-commit-id: 588cc51ac4a006418d973c1713cb616016d855c4

--- app/controllers/concerns/chat.rb
+++ app/controllers/concerns/chat.rb
@@ -1,6 +1,7 @@
 module Chat
-	def self.guest_display_name(user = nil)
+	def self.guest_display_name(user = nil, guest_name = nil)
 		return user.name if user
-		return $funny_names["names"][rand($funny_names["names"].length - 1)] + $funny_names["surnames"][rand($funny_names["surnames"].length - 1)] + rand(99).to_s
-	end
+    return "#{guest_name}_anon#{rand(99).to_s}" if guest_name != ""
+    return $funny_names["names"][rand($funny_names["names"].length - 1)] + $funny_names["surnames"][rand($funny_names["surnames"].length - 1)] + "_anon#{rand(99).to_s}"
+  end
 end

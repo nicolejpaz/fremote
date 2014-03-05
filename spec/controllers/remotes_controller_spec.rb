dcea5c6f71d3eb5bfee497da312b7443243159e6
Nicole Paz <nicole@nic.gateway.2wire.net>
2014-03-05 01:00:53 -0800

    Add and refactor tests for RemotesController.
    Former-commit-id: 22c142b2695563ec619632ca6bb04e766e330122

--- spec/controllers/remotes_controller_spec.rb
+++ spec/controllers/remotes_controller_spec.rb
@@ -2,15 +2,20 @@ require 'spec_helper'
 
 describe RemotesController do
   before(:each) do
-    @sample_user = User.create name: "john", email: "john@john.com", password: "password"
-    @sample_video = "http://www.youtube.com/watch?v=NX_23r7vYak"
-    @sample_video_name = "Test name"
-    @sample_video_description = "Test description"
+    @sample_user = create(:user)
+    @another_user = create(:user, name: "bob", email: "bob@bob.com")
+
+    @params = {
+      name: "Test name",
+      alternate_name: "Alternate name",
+      description: "Test description",
+      video_url: "http://www.youtube.com/watch?v=NX_23r7vYak"
+    }
 
     @sample_remote = Remote.make
-    @sample_remote.populate('http://www.youtube.com/watch?v=NX_23r7vYak')
-    @sample_remote.name = "Test name"
-    @sample_remote.description = "Test description"
+    @sample_remote.populate(@params[:video_url])
+    @sample_remote.name = @params[:name]
+    @sample_remote.description = @params[:description]
     @sample_remote.save
   end
 
@@ -22,26 +27,38 @@ describe RemotesController do
   end
 
   describe "POST create" do
-    it "gets the current user if there is one" do
-      controller.stub(:current_user){@sample_user}
-      post :create, video_url: @sample_video, name: @sample_video_name, description: @sample_video_description
-      expect(assigns(:user)).to eq(@sample_user)
-    end
-
     it "creates a remote with a valid url" do
       count = Remote.all.count
-      post :create, video_url: @sample_video, name: @sample_video_name, description: @sample_video_description
+      post :create, video_url: @params[:video_url], name: @params[:name], description: @params[:description]
       expect(assigns(:remote).remote_id.length).to eq(10)
       expect(Remote.all.count).to eq(count + 1)
     end
 
-    xit "doesn't create a remote with an invalid url" do
-      count = Remote.all.count
-      post :create, video_url: "junk"
-      expect(Remote.all.count).should_not eq(count + 1)
+    context "when a user is logged in" do
+      before(:each) do
+        controller.stub(:current_user){@sample_user}
+        post :create, video_url: @params[:video_url], name: @params[:name], description: @params[:description]
+      end
+
+      it "assigns the current user to @user" do
+        expect(assigns(:user)).to eq @sample_user
+      end
+
+      it "should contain a remote with the current user as the remote owner" do
+        expect(Remote.last.user).to eq @sample_user
+      end
+    end
+
+    context "when a user is not logged in" do
+      before(:each) do
+        post :create, video_url: @params[:video_url], name: @params[:name], description: @params[:description]
+      end
+
+      it "does not assign @user" do
+        expect(assigns(:user)).to be_nil
+      end
     end
   end
-
 
   describe "GET show" do
     it "retrieves @remote from remote_id" do
@@ -80,59 +97,66 @@ describe RemotesController do
 
   describe "PUT control" do
     before(:each) do
-      @sample_owned_remote = @sample_user.remotes.make
-      @sample_owned_remote.populate("http://www.youtube.com/watch?v=NX_23r7vYak")
+      @sample_owned_remote = Remote.make(@sample_user)
+      @sample_owned_remote.populate(@params[:video_url])
+      @sample_owned_remote.admin_only = true
       @sample_owned_remote.save
-      @another_user = User.create name: "bob", email: "bob@bob.com", password: "password"
     end
 
-    it "retrieves @remote from remote_id" do
-      put :control, id: @sample_remote.remote_id
-      expect(assigns(:remote)).to eq(@sample_remote)
-    end
-
-    it "gets the current user if there is one" do
-      controller.stub(:current_user){@sample_user}
-      put :control, id: @sample_remote.remote_id
-      expect(assigns(:user)).to eq(@sample_user)
-    end
-
-    it "updates the remote if owner is logged in" do
-      controller.stub(:current_user){@sample_user}
-      put :control, remote: { :admin_only => "true" }, id: @sample_owned_remote.remote_id
-      expect(assigns(:remote).admin_only).to eq(true)
-    end
-
-    xit "doesn't update the remote if owner is not logged in" do
-      @sample_owned_remote.admin_only = false
-      @sample_owned_remote.save
-      sign_out @sample_user
-      sign_out
-      # sign_in @another_user
-      controller.stub(:current_user) { @another_user }
-      # controller.stub(:current_user){@another_user}
-      put :control, remote: { :admin_only => "true" }, id: @sample_owned_remote.remote_id
-      expect(assigns(:remote).admin_only).to eq(false)
-    end
-  end
-
-  describe "POST chat" do
-    xit "retrieves @remote from remote_id" do
-      post :chat, id: @sample_remote.remote_id
-      expect(assigns(:remote)).to eq(@sample_remote)
-    end
-
-    xit "receives and dispatches a chat message notification" do
-      controller.stub(:current_user){@sample_user}
-      message = nil
-      ActiveSupport::Notifications.subscribe("chat:#{@sample_remote.remote_id}") do |name, start, finish, id, payload|
-          message = payload
+    context "when the current user is the remote owner" do
+      before(:each) do
+        controller.stub(:current_user){@sample_user}
+        put :control, remote: { :admin_only => "false" }, id: @sample_owned_remote.remote_id
       end
-      post :chat, id: @sample_remote.remote_id, chat_message: "dummy message", name: "dummy"
-      until message != nil do
+
+      it "retrieves @remote from remote_id" do
+        expect(assigns(:remote).remote_id).to eq @sample_owned_remote.remote_id
       end
-      ActiveSupport::Notifications.unsubscribe("chat:#{@sample_remote.remote_id}")
-      expect(JSON.parse(message)["message"]).to eq("dummy message")
+
+      it "gets the current user if there is one" do
+        expect(assigns(:user)).to eq @sample_user
+      end
+
+      it "updates the remote" do
+        expect(assigns(:remote).admin_only).to eq(false)
+      end
+    end
+
+    context "when the current user is not the remote owner" do
+      before(:each) do
+        controller.stub(:current_user){@another_user}
+        put :control, remote: { :admin_only => "false" }, id: @sample_owned_remote.remote_id
+      end
+
+      it "gets the current user if there is one" do
+        expect(assigns(:user)).to eq @another_user
+      end
+
+      it "retrieves @remote from remote_id" do
+        expect(assigns(:remote).remote_id).to eq @sample_owned_remote.remote_id
+      end 
+
+      it "does not update the remote" do
+        expect(assigns(:remote).admin_only).to eq true
+      end
+    end
+
+    context "when the current user is a guest" do
+      before(:each) do
+        put :control, remote: { :admin_only => "false" }, id: @sample_owned_remote.remote_id
+      end
+
+      it "does not assign @user" do
+        expect(assigns(:user)).to be_nil
+      end
+
+      it "retrieves @remote from remote_id" do
+        expect(assigns(:remote).remote_id).to eq @sample_owned_remote.remote_id
+      end 
+
+      it "does not update the remote" do
+        expect(assigns(:remote).admin_only).to eq true
+      end
     end
   end
 
@@ -143,23 +167,137 @@ describe RemotesController do
     end
   end
 
-  describe 'PATCH update' do
+  describe "PATCH update" do
     before(:each) do
-      @sample_owned_remote = @sample_user.remotes.make
-      @sample_owned_remote.populate("http://www.youtube.com/watch?v=NX_23r7vYak")
+      @sample_owned_remote = Remote.make(@sample_user)
+      @sample_owned_remote.populate(@params[:video_url])
+      @sample_owned_remote.name = @params[:name]
+      @sample_owned_remote.description = @params[:description]
+      @sample_owned_remote.save      
+    end
+
+    context "when the current user is the remote owner" do
+      before(:each) do
+        controller.stub(:current_user){@sample_user}
+        patch :update, id: @sample_owned_remote.remote_id, name: @params[:alternate_name]
+      end
+
+      it "gets the current user if there is one" do
+        expect(assigns(:user)).to eq @sample_user 
+      end
+
+      it "retrieves @remote from remote_id" do
+        expect(assigns(:remote).remote_id).to eq @sample_owned_remote.remote_id
+      end
+
+      it "assigns the current user to @remote_owner" do
+        expect(assigns(:remote_owner)).to eq @sample_user
+      end
+
+      it "updates the remote based off of the given parameters" do
+        expect(Remote.last.name).to eq @params[:alternate_name]
+      end
+    end
+
+    context "when the current user is not the remote owner" do
+      before(:each) do
+        controller.stub(:current_user){@another_user}
+        patch :update, id: @sample_owned_remote.remote_id, name: @params[:alternate_name]
+      end
+
+      it "assigns the current user to @user" do
+        expect(assigns(:user)).to eq @another_user
+      end
+
+      it "retrieves @remote from remote_id" do
+        expect(assigns(:remote).remote_id).to eq @sample_owned_remote.remote_id
+      end
+
+      it "does not assign @remote_owner" do
+        expect(assigns(:remote_owner)).to be_nil
+      end
+
+      it "does not update the remote's attributes" do
+        expect(Remote.last.name).to_not eq @params[:alternate_name]
+      end
+    end
+
+    context "when the current user is a guest" do
+      before(:each) do
+        patch :update, id: @sample_owned_remote.remote_id, name: @params[:alternate_name]
+      end
+
+      it "does not assign @user" do
+        expect(assigns(:user)).to be_nil
+      end
+
+      it "retrieves @remote from remote_id" do
+        expect(assigns(:remote).remote_id).to eq @sample_owned_remote.remote_id
+      end
+
+      it "does not assign @remote_owner" do
+        expect(assigns(:remote_owner)).to be_nil
+      end
+      
+      it "does not update the remote's attributes" do
+        expect(Remote.last.name).to_not eq @params[:alternate_name]
+      end
+    end
+  end
+
+  describe 'GET edit' do
+    before(:each) do
+      @sample_owned_remote = Remote.make(@sample_user)
+      @sample_owned_remote.populate(@params[:video_url])
       @sample_owned_remote.save
-      @another_user = User.create name: "bob", email: "bob@bob.com", password: "password"
     end
 
-    it "gets the current user if there is one" do
-      controller.stub(:current_user){@sample_user}
-      patch :update, id: @sample_remote.remote_id
-      expect(assigns(:user)).to eq(@sample_user)
+    it 'assigns the current remote to @remote' do
+      get :edit, id: @sample_owned_remote.remote_id
+      expect(assigns(:remote)).to eq @sample_owned_remote
     end
 
-    it "retrieves @remote from remote_id" do
-      patch :update, id: @sample_remote.remote_id
-      expect(assigns(:remote)).to eq(@sample_remote)
+    context 'when the current user is logged in' do
+      context 'and the current user is also the remote owner' do
+        before(:each) do
+          controller.stub(:current_user){@sample_user}
+          get :edit, id: @sample_owned_remote.remote_id
+        end
+
+        it 'assigns the current user to @user' do
+          expect(assigns(:user)).to eq @sample_user
+        end
+
+        it 'assigns the current user to @remote_owner' do
+          expect(assigns(:remote_owner)).to eq @sample_user
+        end
+      end
+
+      context 'and the current user is not the remote owner' do
+        before(:each) do
+          @another_user = User.create name: "jane", email: "jane@jane.com", password: "password"
+          controller.stub(:current_user){@another_user}
+          get :edit, id: @sample_owned_remote.remote_id
+        end
+
+        it 'assigns the current user to @user' do
+          expect(assigns(:user)).to eq @another_user
+        end
+
+        it 'does not assign the current user to @remote_owner' do
+          expect(assigns(:remote_owner)).to eq nil
+        end
+      end
+    end
+
+    context 'when the current user is a guest' do
+      before(:each) do
+        get :edit, id: @sample_owned_remote.remote_id
+      end
+
+      it 'does not assign @user' do
+        expect(assigns(:user)).to eq nil
+      end
     end
   end
 end

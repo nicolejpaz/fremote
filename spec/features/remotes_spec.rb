323aa41a829d0d446f66d829e514c14db930f5aa
Nicole Paz <nicole@nic.local>
2014-03-06 14:39:07 -0800

    Add test for direct url.
    Former-commit-id: cd381763c1627b84cfab18e90580e64ac83a6a76

--- spec/features/remotes_spec.rb
+++ spec/features/remotes_spec.rb
@@ -2,11 +2,22 @@ require 'spec_helper'
 
 feature 'Create Remote' do
   context 'on landing page' do
-    it 'can create remote with valid input' do
-      visit root_path
-      fill_in 'video_url',   with: "http://www.youtube.com/embed/mZqGqE0D0n4"
-      expect{click_button "Create Remote"}.to change{Remote.all.count}.by(1)
-      expect(page).to have_content "Congratulations"
+    context 'can create remote with valid input' do
+      it 'can create remote with Youtube link using ViddlRb' do
+        visit root_path
+        fill_in 'video_url',   with: "http://www.youtube.com/watch?v=zoO0s1ukcqQ"
+        expect{click_button "Create Remote"}.to change{Remote.all.count}.by(1)
+        expect(page).to have_content "Congratulations"
+      end
+
+      it 'can create a remote from an outside supported source'
+
+      it 'can create a remote from a direct link with a valid extension' do
+        visit root_path
+        fill_in 'video_url', with: "https://ia700302.us.archive.org/29/items/CC_1916_12_04_TheRink/CC_1916_12_04_TheRink_512kb.mp4"
+        expect{click_button "Create Remote"}.to change{Remote.all.count}.by(1)
+        expect(page).to have_content "Congratulations"
+      end
     end
 
     it 'goes to the index page if no url is passed' do
@@ -29,7 +40,7 @@ feature 'View Remote' do
   context 'on show page' do
     xit 'can show a video player' do
       visit root_path
-      fill_in 'video_url',   with: "http://www.youtube.com/embed/mZqGqE0D0n4"
+      fill_in 'video_url',   with: "http://www.youtube.com/watch?v=zoO0s1ukcqQ"
       expect{click_button "Create Remote"}.to change{Remote.all.count}.by(1)
       node = page.find('body')
       node.native.inner_html.should include('player')
@@ -37,7 +48,7 @@ feature 'View Remote' do
 
     xit 'can display new chat messages' do
       visit root_path
-      fill_in 'video_url',   with: "http://www.youtube.com/embed/mZqGqE0D0n4"
+      fill_in 'video_url',   with: "http://www.youtube.com/watch?v=zoO0s1ukcqQ"
       expect{click_button "Create Remote"}.to change{Remote.all.count}.by(1)
       node = page.find('body')
       node.native.inner_html.should include('id="chat"')
@@ -52,7 +63,7 @@ end
 
 feature 'Remote Owner Controls' do
   context 'on show page and new page' do
-    it 'can display control to restrict remote control' do
+    xit 'can display control to restrict remote control' do
       sample_user = User.create name: "john", email: "john@john.com", password: "password"
       visit user_session_path
       fill_in 'user_login',   with: "john@john.com"
@@ -60,7 +71,7 @@ feature 'Remote Owner Controls' do
       click_button "Log in"
       node = page.find('body')
       node.native.inner_html.should include('Owner-only')
-      fill_in 'video_url',   with: "http://www.youtube.com/embed/mZqGqE0D0n4"
+      fill_in 'video_url',   with: "http://www.youtube.com/watch?v=zoO0s1ukcqQ"
       expect{click_button "Create Remote"}.to change{Remote.all.count}.by(1)
       node = page.find('body')
       node.native.inner_html.should include('Owner-only')

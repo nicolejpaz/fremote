376e8a75aaf9788cc9dd0889cadbf549c80c8b3c
Nicole Paz <nicole@nic.local>
2014-03-08 15:13:22 -0800

    Refactor landing.ui so it doesn't repeat itself.
    Former-commit-id: a5b1a4defa383f04d8ab2db9c4807e0f264a0573

--- app/assets/javascripts/landing.ui.js
+++ app/assets/javascripts/landing.ui.js
@@ -1,9 +1,12 @@
 window.onload = function() {
-  var windowHeight = $(window).height()
-  $('#center_column').css('padding-top', windowHeight / 3)
+  changeWindowHeight()
 }
 
 window.onresize = function() {
+  changeWindowHeight()
+}
+
+function changeWindowHeight() {
   var windowHeight = $(window).height()
   $('#center_column').css('padding-top', windowHeight / 3)
 }

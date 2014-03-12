84b678646b2e493764717e15fa13c8a6da029182
Nicole Paz <nicole@nic.local>
2014-03-12 16:58:02 -0700

    Add key that says who's an owner and who's a member. Switched the owner and member icons because a star seems to be more important than a user icon (subjective).

--- app/assets/javascripts/remotes.js
+++ app/assets/javascripts/remotes.js
@@ -229,9 +229,9 @@ player.ready(function(){
 
 function displayWatcher(watcher) {
   if (watcher.user_kind === 'owner') {
-    $('#watchers').append('<li id="' + watcher.username.toLowerCase() + '" class="' + watcher.user_kind + '">' + watcher.username + '<span class="glyphicon glyphicon-user owner"></span></li>')
+    $('#watchers').append('<li id="' + watcher.username.toLowerCase() + '" class="' + watcher.user_kind + '">' + watcher.username + '<span class="glyphicon glyphicon-star owner"></span></li>')
   } else if (watcher.user_kind === 'member') {
-    $('#watchers').append('<li id="' + watcher.username.toLowerCase() + '" class="' + watcher.user_kind + '">' + watcher.username + '<span class="glyphicon glyphicon-star member"></span></li>')
+    $('#watchers').append('<li id="' + watcher.username.toLowerCase() + '" class="' + watcher.user_kind + '">' + watcher.username + '<span class="glyphicon glyphicon-user member"></span></li>')
   } else {
     $('#watchers').append('<li id="' + watcher.username.toLowerCase() + '" class="' + watcher.user_kind + '">' + watcher.username + '</li>')
   }

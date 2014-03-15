0b59a81238550d27fbca33a9fdb0553dd43ad380
Nicole Paz <nicole@nic.local>
2014-03-15 16:34:36 -0700

    Add canvas to application and fremote.js.
    Former-commit-id: 58025bed993da8f95070001bebb6e3c9403ede7c

--- app/assets/javascripts/fremote.js
+++ app/assets/javascripts/fremote.js
@@ -3,5 +3,6 @@ $(document).on('ready', function(){
   var remote = new Remote(stream.source)
   var player = new Player(stream.source, remote)
   var chat = new Chat(stream.source, remote)
+  var canvas = new Canvas(stream.source, remote)
   var playlist = new Playlist(stream.source, remote)
 })

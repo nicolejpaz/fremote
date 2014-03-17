240658c948f41e3e1ddeb2759e83a917046ccc38
Nicole Paz <nicole@nic.gateway.pace.com>
2014-03-17 15:42:22 -0700

    Add character limit check to chat submission. This is to reset the character limit check to 500 characters.
    Former-commit-id: b1a920ce1acf029afd5b514c8c70d06c06d4fa87

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -19,14 +19,13 @@ function Chat(source,remote){
     var chatBtn = $('input.btn[name="commit"]')
     var chatForm = $('form').first()
 
-    characterLimitELement.html(charactersRemaining + ' characters left')
+    characterLimitElement.html(charactersRemaining + ' characters left')
 
     if (charactersRemaining < 0) {
       self.addError(chatForm, chatBox, chatBtn)
     } else {
       self.removeError(chatForm, chatBox, chatBtn)
     }
-
   }
 
   self.addError = function(chatForm, chatBox, chatBtn) {
@@ -49,50 +48,6 @@ function Chat(source,remote){
     var data = JSON.parse(event.data)
     chatInput.val('')
     chatTableBody.prepend('<tr>' + '<td>' + data.message + '</td>' + '<td class="grey-text">' + data.name + '</td>' + '</tr>')
+    self.checkCharacterLimit(chatInput.val())
   }
-
 } // END CHAT CONSTRUCTOR
-
-
-
-// $(document).ready(function() {
-//   $('input#chat_message').on('keyup', function() {
-//     checkCharacterLimit(this.value)
-//   })
-// })
-
-// function checkCharacterLimit(chatValue) {
-//   var charactersRemaining = getCharactersRemaining(chatValue.length)
-//   var chatBox = $('div#chat_character_limit')
-//   var chatBtn = $('input.btn[name="commit"]')
-//   var chatForm = $('form').first()
-
-//   $('div#chat_character_limit').html(charactersRemaining + ' characters left')
-//   if (charactersRemaining < 0) {
-//     addError(chatForm, chatBox, chatBtn)
-//   } else {
-//     removeError(chatForm, chatBox, chatBtn)
-//   }
-// }
-
-// function addError(chatForm, chatBox, chatBtn) {
-//   chatForm.addClass('has-error')
-//   chatBox.addClass('error')
-//   chatBtn.prop('disabled', true)
-// }
-
-// function removeError(chatForm, chatBox, chatBtn) {
-//   chatForm.removeClass('has-error')
-//   chatBox.removeClass('error')
-//   chatBtn.prop('disabled', false)
-// }
-
-// function getCharactersRemaining(chatLength) {
-//   return 500 - chatLength
-// }
-
-// function sendChatMessage(event) {
-//   var data = JSON.parse(event.data)
-//   $('#chat_message').val('')
-//   $('#chat_table_body').prepend('<tr>' + '<td>' + data.message + '</td>' + '<td class="grey-text">' + data.name + '</td>' + '</tr>')
-// }

41114d914378ab0e6a53bb5e60ef3bc797654540
Nicole Paz <nicole@michelle-hp.wlan.guest.canyons.edu>
2014-03-17 13:38:59 -0700

    Add pluggable_js code.

---
+++ app/assets/javascripts/remotes.js
@@ -0,0 +1,17 @@
+window['remotes#show'] = function(data) {
+  $(document).on('ready', function() {
+    var stream = new Stream()
+    var remote = new Remote()
+    var player = new Player(stream.source, remote)
+    var chat = new Chat(stream.source, remote)
+    var canvas = new Canvas(stream.source, remote)
+    var playlist = new Playlist(stream.source, remote)
+    var watcher_list = new WatcherList(stream.source, remote)
+  })
+}
+
+window['remotes#edit'] = function(data) {
+  $(document).on('ready', function() {
+    var remote = new Remote()
+  })
+}

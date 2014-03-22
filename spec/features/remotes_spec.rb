878ef413250c6c677bc0de70977cf2413381f254
Nicole Paz <nicole@nic.local>
2014-03-22 15:39:07 -0700

    Rename guest_features_spec to remotes_spec. Separate sessions testing to sessions_spec.

--- spec/features/remotes_spec.rb
+++ spec/features/remotes_spec.rb
@@ -1,39 +1,6 @@
 require 'spec_helper'
 
 feature 'When a guest visits the landing page' do
-  scenario 'they can see the user session links in the navigation' do
-    visit root_path
-
-    expect(page).to have_content('Login')
-    expect(page).to have_content('Sign up')
-  end
-
-  scenario 'they can sign in and out' do
-    visit root_path
-
-    click_link 'Sign up'
-
-    fill_in 'user[name]', :with => 'test'
-    fill_in 'user[email]', :with => 'test@test.com'
-    fill_in 'user[password]', :with => 'pa55word3'
-    fill_in 'user[password_confirmation]', :with => 'pa55word3'
-    click_button 'Sign up'
-
-    expect(page).to have_content('Welcome, test')
-
-    click_link 'Logout'
-
-    expect(page).to have_content('Login')
-
-    click_link 'Login'
-
-    fill_in 'user[login]', :with => 'test'
-    fill_in 'user[password]', :with => 'pa55word3'
-    click_button 'Log in'
-
-    expect(page).to have_content('Welcome, test')
-  end 
-
   scenario 'they can create a quick remote' do
     visit root_path
 

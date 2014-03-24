afb71465c7cd8c7e46c6ee6695fab200f508e373
benjamin <benjamin@benjamin.(none)>
2014-03-24 13:02:26 -0700

    Add provider logos to landing page.  Create custom error pages with errors controller and views.

--- config/routes.rb
+++ config/routes.rb
@@ -14,67 +14,13 @@ Fremote::Application.routes.draw do
   get 'time' => 'remotes#time'
   put 'remotes/:id/control' => 'remotes#control'
 
-  %w( 404 422 500 ).each do |code|
-    get code, :to => "errors#show", :code => code
-  end
-
   resources :remotes do
     resource :playlist, only: [:update, :show, :create, :destroy]
     resource :chat, controller: 'chat', only: [:create, :update]
   end
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  match "/404", to: "errors#404_not_found", via: [:get, :post, :patch, :put, :delete]
+  match "/422", to: "errors#422_unprocessable_entity", via: [:get, :post, :patch, :put, :delete]
+  match "/500", to: "errors#500_internal_server_error", via: [:get, :post, :patch, :put, :delete]
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end

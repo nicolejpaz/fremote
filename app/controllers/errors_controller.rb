52a38a9af03327f3b639b720216c15ff5d8a6d59
benjamin <benjamin@benjamin.(none)>
2014-03-24 13:02:26 -0700

    Add provider logos to landing page.  Create custom error pages with errors controller and views.
    Former-commit-id: afb71465c7cd8c7e46c6ee6695fab200f508e373

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,7 +1,15 @@
 class ErrorsController < ApplicationController
  
-  def show
-    render status_code.to_s, :status => status_code
+  def not_found
+    render 'errors/404_not_found'
+  end
+
+  def unprocessable_entity
+    render 'errors/422_unprocessable_entity'
+  end
+
+  def internal_server_error
+    render 'errors/500_internal_server_error'
   end
  
 protected

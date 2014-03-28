be826fff1a91f5a0f397c6601f7026485b79febc
Nicole Paz <nicole@nic.local>
2014-03-28 15:43:48 -0700

    Fix tests.

--- spec/factories/remotes.rb
+++ spec/factories/remotes.rb
@@ -3,7 +3,7 @@ require 'faker'
 FactoryGirl.define do
   factory :remote do |f|
     f.url         "http://www.youtube.com/embed/mZqGqE0D0n4"
-    f.name        Faker::Lorem.sentence
-    f.description Faker::Lorem.paragraph
+    f.name        Faker::Lorem.sentence.slice(1..60)
+    f.description Faker::Lorem.paragraph.slice(1..5000)
   end
 end

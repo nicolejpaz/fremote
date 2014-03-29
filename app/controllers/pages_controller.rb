02e5b979249034c0147a42f5cb518626ca3478b0
benjamin <benjamin@benjamin.(none)>
2014-03-28 17:53:56 -0700

    Move sitemap to pages controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,4 +9,10 @@ class PagesController < ApplicationController
   def terms_of_use
   end
 
+  def sitemap
+      respond_to do |format|
+        format.xml { render 'pages/sitemap.xml', layout: false }
+      end
+  end
+
 end

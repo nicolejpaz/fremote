93ab84463f29e17cb6e469a846e400350ebc31f8
Nicole Paz <nicole@nic.gateway.2wire.net>
2014-03-30 16:01:41 -0700

    Add Solve Media to view.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
+  prepend_before_filter :require_no_authentication, only: [ :new, :create, :cancel ]
+  prepend_before_filter :authenticate_scope!, only: [:edit, :update, :destroy]
+
   def create
-    p verify_solvemedia_puzzle
-    if verify_solvemedia_puzzle(:error_message => 'Solve Media puzzle input is invalid')
+    if verify_solvemedia_puzzle
       super
     else
-      # error
+      build_resource(sign_up_params)
+      resource.valid?
+      resource.errors.add(:base, "There was a problem with the Solve Media code below. Please reenter the code.")
+      clean_up_passwords(resource)
+      # respond_with_navigational(resource) { render_with_scope :new }
+      respond_with(resource)
+      # flash.now[:alert] = "There was a problem with the Solve Media code below. Please reenter the code."
+      # render :new
+      # clean_up_passwords resource
+      # respond_with resource
     end
   end
 end

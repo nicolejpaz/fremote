f4a4f3c48fc79962446d1fbc6595a8efdbba4c3b
Nicole Paz <nicole@nic.gateway.2wire.net>
2014-03-30 17:32:47 -0700

    Add Solve Media error to Devise errors.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,12 +10,7 @@ class RegistrationsController < Devise::RegistrationsController
       resource.valid?
       resource.errors.add(:base, "There was a problem with the Solve Media code below. Please reenter the code.")
       clean_up_passwords(resource)
-      # respond_with_navigational(resource) { render_with_scope :new }
       respond_with(resource)
-      # flash.now[:alert] = "There was a problem with the Solve Media code below. Please reenter the code."
-      # render :new
-      # clean_up_passwords resource
-      # respond_with resource
     end
   end
 end
